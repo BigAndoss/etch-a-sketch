af1a1fb2d7198d8673afb7466d9d7d9fbdab6c46
andoss <anditahiri460@gmail.com>
2024-03-11 21:23:37 +0100

    Finishet v1

---
+++ script.js
@@ -0,0 +1,30 @@
+const btn = document.getElementById("start")
+
+let pixels;
+btn.addEventListener("click", () =>{
+    pixels = prompt("Enter Pixel Number (Max 100)")
+    canvas(pixels);
+} )
+
+const container = document.querySelector('.container');
+
+const box = document.querySelector(".box")
+let color = box.id
+console.log(color)
+
+const canvas = (pixels) =>{
+    for(let i =0; i<pixels*pixels; i++){
+        const a = document.createElement('div')
+        a.className = "boxy"
+        a.style.width= 1000/pixels+"px"
+        a.style.height= 1000/pixels+"px"
+        a.addEventListener('mouseover', ()=>{
+            a.style.backgroundColor=color
+        })
+        container.appendChild(a)
+    }
+}
+
+function colorPicker (c){
+    color = c
+}
